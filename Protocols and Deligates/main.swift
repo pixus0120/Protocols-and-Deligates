a332dee197f4129d0220bd43cf392965483be9b6
pixus <pix_us@mail.ru>
2021-05-18 22:24:26 -0400

    final commit

--- Protocols and Deligates/main.swift
+++ Protocols and Deligates/main.swift
@@ -1,11 +1,62 @@
-//
-//  main.swift
-//  Protocols and Deligates
-//
-//  Created by locussigilli on 4/14/21.
-//
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?   // must have adnancedLifeSupport  !!!!
+    
+    func assessSituation() {
+        print("Can tell me what happend?")
+    }
+    func medicalEmergency() {
+        delegate?.performCPR()
+        print("emergency situation")
+    }
+}
+
+
+struct Paramedic: AdvancedLifeSupport {   // must have adnancedLifeSupport
+    init(handler: EmergencyCallHandler) {    //!!!!
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The Paramedic does chest commpression")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The Doctor does chest comp")
+    }
+    
+    func useScopes() {                          // +  method
+        print(" linsten the heart")
+    }
+}
+
+
+class Surgeon: Doctor {                // subclass from doctor
+    override func performCPR() {
+        super.performCPR()
+        print("singing")
+    }
+    func useInstruments() {
+        print("Instruments")
+    }
+
+}
+
+let emelio = EmergencyCallHandler()
+
+let angela = Surgeon(handler: emelio)
+angela.useInstruments()
+let john = Doctor(handler: emelio)
+let pete = Paramedic(handler: emelio)    //!!!
+
+pete.
+emelio.medicalEmergency()
 
